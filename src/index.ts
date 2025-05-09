0b6fc88d4d343acdc22f59cb19e6a538af21d1cd
Lautaro Di Sanza <lautarod@gmail.com>
2025-05-09 10:19:33 -0300

    Re create group

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
 import { Group } from "@semaphore-protocol/group"
 import { Identity } from "@semaphore-protocol/identity"
-import { poseidon2 } from "poseidon-lite"
-import { CompiledCircuit } from "@noir-lang/noir_js"
 import { generateNoirProof } from "@semaphore-protocol/proof"
 import { verifyNoirProof } from "@semaphore-protocol/proof"
+import { SemaphoreSubgraph } from "@semaphore-protocol/data"
+import { SupportedNetwork } from "@semaphore-protocol/utils"
 
+const reCreateGroup = async (idGroup:string,network:SupportedNetwork) => {
+    const semaphoreSubgraph = new SemaphoreSubgraph(network)
+
+    const { members } = await semaphoreSubgraph.getGroup(idGroup, { members: true })
+
+    return members
+}
 async function main() {
-    const merkleTreeDepth = 10
 
     const message = "Hello world"
     const scope = "Scope"
 
     const identity = new Identity("secret")
 
-    const group = new Group([1n, 2n, identity.commitment])
-    const proof = await generateNoirProof(identity, group, message, scope, merkleTreeDepth)
+    // Re create group 
+    const members = await reCreateGroup("0",'scroll-sepolia')
+
+    const group = new Group(members)
+
+     group.addMember(identity.commitment)
+
+    const proof = await generateNoirProof(identity, group, message,group.root)
     const isValid = await verifyNoirProof(proof)
     console.log("🚀 ~ main ~ isValid:", isValid)
 
